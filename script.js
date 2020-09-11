4573574a1888a6980dd943b57c059ad9fc7c30a9
Rasmus Thinghuus Toft <rasmustoft@Rasmuss-MacBook-Pro.local>
2020-09-11 11:02:32 +0200

    images done

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function loadJSON() {
 
 function visCovers() {
     const templatePointer = document.querySelector("template");
-    const listPointer = document.querySelector("#list");
+    const listPointer = document.querySelector(".item-c");
     listPointer.innerHTML = "";
     console.log(covers);
 
@@ -22,12 +22,12 @@ function visCovers() {
             const minKlon = templatePointer.cloneNode(true).content;
             minKlon.querySelector(".kunstner").textContent = cov.gsx$kunstner.$t;
             minKlon.querySelector(".albumnavn").textContent = cov.gsx$albumnavn.$t;
-            minKlon.querySelector(".år").textContent = cov.gsx$år.$t;
+            //            minKlon.querySelector(".udgivelse").textContent = cov.gsx$udgivelse.$t;
             minKlon.querySelector(".pladeselskab").textContent = cov.gsx$pladeselskab.$t;
             //minKlon.querySelector(".").textContent = cov.gsx$lang.$t;
             minKlon.querySelector(".kategori").textContent = cov.gsx$kategori.$t;
             minKlon.querySelector(".beskrivelse").textContent = cov.gsx$beskrivelse.$t;
-            minKlon.querySelector(".billede").src = `imgs/small/${cov.gsx$billede.$t}` + "-sm.jpg";
+            minKlon.querySelector(".billede").src = `imgs/${cov.gsx$billede.$t}` + ".jpg";
 
             minKlon.querySelector("article").addEventListener("click", () => visDetaljer(cov));
 
@@ -43,13 +43,13 @@ function visDetaljer(cov) {
     popup.style.display = "block";
 
     popup.querySelector(".pop_kunstner").textContent = cov.gsx$kunstner.$t
-    popup.querySelector(".pop_år").textContent = cov.gsx$år.$t;
+    popup.querySelector(".pop_udgivelse").textContent = cov.gsx$udgivelse.$t;
     //popup.querySelector(".pop_").textContent = cov.gsx$kort.$t;
     popup.querySelector(".pop_pladeselskab").textContent = cov.gsx$pladeselskab.$t;
     popup.querySelector(".pop_kategori").textContent = cov.gsx$kategori.$t;
     popup.querySelector(".pop_beskrivelse").textContent = cov.gsx$beskrivelse.$t;
     popup.querySelector(".pop_albumnavn").textContent = cov.gsx$albumnavn.$t;
-    popup.querySelector(".pop_billede").src = `imgs/small/${cov.gsx$billede.$t}` + "-sm.jpg";
+    popup.querySelector(".pop_billede").src = `imgs/${cov.gsx$billede.$t}` + ".jpg";
 }
 
 document.querySelector("#luk").addEventListener("click", () => popup.style.display = "none");
